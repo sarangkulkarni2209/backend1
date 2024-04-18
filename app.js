900fbef3e258de90809cf102e98b090b2a7ba8f9
sarangkulkarni2209 <sarang.vk2000@gmail.com>
2024-04-18 22:46:05 +0530

    json data added

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const data = {
+    "name":"John", 
+    "age":30, 
+    "car":null
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -13,6 +19,9 @@ app.get('/twitter', (req, res) =>{
     res.send('sarang2209')
 })
 
+app.get('/info', (req, res) => {
+    res.json(data)
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 }) 
